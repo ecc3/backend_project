dbc99c5686ddcce7f8c1af48142f21a3c083861c
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-11-25 12:49:51 +0000

    created up migration to create table topics

---
+++ db/migrations/20191125123727_create_table_topics.js
@@ -0,0 +1,9 @@
+exports.up = function(knex) {
+  console.log("creating table topics");
+  return knex.schema.createTable("topics", topicsTable => {
+    topicsTable.string("slug").primary();
+    topicsTable.string("description");
+  });
+};
+
+exports.down = function(knex) {};
