6081b885deaa51d4eabc5707b5d065878b7debe6
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-11-25 12:15:49 +0000

    added index files in data, dev-data and test-data

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,30 @@
-const ENV = process.env.NODE_ENV || 'development';
+const { user } = require("./config");
+const { password } = require("./config");
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
+  client: "pg",
   migrations: {
-    directory: './db/migrations'
+    directory: "./db/migrations"
   },
   seeds: {
-    directory: './db/seeds'
+    directory: "./db/seeds"
   }
 };
 
 const customConfig = {
   development: {
     connection: {
-      database: 'nc_news'
-      // user,
-      // password
+      database: "nc_news",
+      user: user,
+      password: password
     }
   },
   test: {
     connection: {
-      database: 'nc_news_test'
-      // user,
-      // password
+      database: "nc_news_test",
+      user: user,
+      password: password
     }
   }
 };
