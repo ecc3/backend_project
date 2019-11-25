e938afe1b871a819b4fe22c90a18ab8e30bd8f76
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-11-25 14:46:18 +0000

    added up migration to create comments table

---
+++ db/migrations/20191125144132_create_table_comments.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  console.log("creating table comments");
+  return knex.schema.createTable("comments", commentsTable => {
+    commentsTable.increments().primary();
+    commentsTable.string("author").references("users.username");
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+    commentsTable.string("body");
+  });
+};
+
+exports.down = function(knex) {};
